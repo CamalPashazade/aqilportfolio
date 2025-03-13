da2845d220075336e740de2584c03746bb294265
CamalPashazade <camal24011992@gmail.com>
2025-03-13 22:46:32 +0400

    lkjfgh

--- src/main.jsx
+++ src/main.jsx
@@ -1,5 +1,4 @@
 import { StrictMode } from 'react'
-import { HashRouter } from "react-router-dom"
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
@@ -8,8 +7,6 @@ import App from './App.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <HashRouter base="/">
     <App />
-    </HashRouter>
   </StrictMode>,
 )
