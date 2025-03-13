f491521bd3e07521dd6bdda0cdaa6c6631e791cd
CamalPashazade <camal24011992@gmail.com>
2025-03-14 02:52:21 +0400

    Initial commit for GitHub Pages deploy

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,10 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   // base:"/aqilportfolio"
-base: '/'
+base: '/',
+build: {
+  outDir: 'dist'
+}
 })
 
 
