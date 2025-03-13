4e2b83fd09b65f46f9c02d2fee993c1eae518629
CamalPashazade <camal24011992@gmail.com>
2025-03-13 22:05:03 +0400

    first commit

--- vite.config.js
+++ vite.config.js
@@ -2,29 +2,28 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // // https://vite.dev/config/
-// export default defineConfig({
-//   plugins: [react()],
-//   // base:"/aqilportfolio"
-//     base: ''
-// })
-
-
 export default defineConfig({
   plugins: [react()],
-  base: './',
-  server: {
-    cors: true,
-    
-    mimeTypes: {
-      'jsx': 'application/javascript', // JSX üçün MIME tipini düzəlt
-      
-    }
-  },
-  build: {
-    outDir: 'dist',
-    assetsDir: 'assets',
-    rollupOptions: {
-      input: 'index.html'
-    }
-  }
+  // base:"/aqilportfolio"
+    // base: ''
 })
+
+
+// export default defineConfig({
+//   plugins: [react()],
+//   base: '',
+//   server: {
+//     cors: true,
+    
+//     mimeTypes: {
+//       'jsx': 'application/javascript', // JSX üçün MIME tipini düzəlt
+      
+//     }
+//   },
+//   build: {
+//     outDir: 'dist',
+//     rollupOptions: {
+//       input: 'index.html'
+//     }
+//   }
+// })
