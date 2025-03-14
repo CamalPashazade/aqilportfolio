9a85e95ae51b740cc2acce0c7c371a52fb850abd
CamalPashazade <camal24011992@gmail.com>
2025-03-14 22:50:47 +0400

    dcddc

--- src/components/layout/Layout.jsx
+++ src/components/layout/Layout.jsx
@@ -21,7 +21,7 @@ const Layout = ({ children }) => {
           <main className={styles.layout}>
             {children}
           </main>
-          <Footer/>
+      <Footer/>
 
         </div>
     
