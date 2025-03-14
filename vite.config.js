2ec96c7613ea2dc8aaaf81dad123651b2b46f014
CamalPashazade <camal24011992@gmail.com>
2025-03-14 05:01:26 +0400

    rgregr

--- vite.config.js
+++ vite.config.js
@@ -1,32 +1,19 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  // base:"/aqilportfolio"
-base: './',
-build: {
-  outDir: 'dist'
-}
+  base: '/', // <- Match the GitHub repo name
+  server: {
+    cors: true,
+    mimeTypes: {
+      'jsx': 'application/javascript'
+    }
+  },
+  build: {
+    outDir: 'dist',
+    rollupOptions: {
+      input: 'index.html'
+    }
+  }
 })
-
-
-// export default defineConfig({
-//   plugins: [react()],
-//   base: '',
-//   server: {
-//     cors: true,
-    
-//     mimeTypes: {
-//       'jsx': 'application/javascript', // JSX üçün MIME tipini düzəlt
-      
-//     }
-//   },
-//   build: {
-//     outDir: 'dist',
-//     rollupOptions: {
-//       input: 'index.html'
-//     }
-//   }
-// })
