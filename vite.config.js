767282d5fddf11c4552df1c7f1a74b36ad590832
CamalPashazade <camal24011992@gmail.com>
2025-03-14 21:54:22 +0400

    Add .nojekyll file

--- vite.config.js
+++ vite.config.js
@@ -3,17 +3,6 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/', // <- Match the GitHub repo name
-  // server: {
-  //   cors: true,
-  //   mimeTypes: {
-  //     'jsx': 'application/javascript'
-  //   }
-  // },
-  // build: {
-  //   outDir: 'dist',
-  //   rollupOptions: {
-  //     input: 'index.html'
-  //   }
-  // }
+  base: '/aqilportfolio/', 
+
 })
