5c2eb909c51d13912297851c05ead84666ce939c
CamalPashazade <camal24011992@gmail.com>
2025-03-14 05:07:20 +0400

    some change

--- vite.config.js
+++ vite.config.js
@@ -4,16 +4,16 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   base: '/', // <- Match the GitHub repo name
-  server: {
-    cors: true,
-    mimeTypes: {
-      'jsx': 'application/javascript'
-    }
-  },
-  build: {
-    outDir: 'dist',
-    rollupOptions: {
-      input: 'index.html'
-    }
-  }
+  // server: {
+  //   cors: true,
+  //   mimeTypes: {
+  //     'jsx': 'application/javascript'
+  //   }
+  // },
+  // build: {
+  //   outDir: 'dist',
+  //   rollupOptions: {
+  //     input: 'index.html'
+  //   }
+  // }
 })
